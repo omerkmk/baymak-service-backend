7b951c6b35bac5ebff007d3d645064d209632458
omerkmk03 <omerkumek03@gmail.com>
2025-12-18 19:59:33 +0300

    securityConfig ve templates dosyaları güncellendi

--- src/main/java/com/baymak/backend/config/SecurityConfig.java
+++ src/main/java/com/baymak/backend/config/SecurityConfig.java
@@ -41,7 +41,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "http://3.238.235.255",
+                "http://3.238.235.255:80"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
